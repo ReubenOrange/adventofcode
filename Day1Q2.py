2e5df2a9a6937d4dabcad908a5f1ad0005487156
ReubenOrange <reuben.orange@gmail.com>
2020-12-01 20:30:16 +1100

    Update Day1Q2.py

--- Day1Q2.py
+++ Day1Q2.py
@@ -4,13 +4,15 @@ split_input_string = input.split()
 split_input_int = [int(i) for i in split_input_string] 
 
 answer_tuple = (0,0)
+remainder_list = []
 
 for x in split_input_int:
-    for y in split_input_int:
+    remainder_list.append(2020-x)
+    for y in remainder_list:
         for z in split_input_int:
-            if x+y+z == 2020:
-                answer_tuple = (x,y,z)
-                break                
+            if x + z == y:
+                answer_tuple = (x,2020-y,z)
+                break
 
 print("Answer tuple: " + str(answer_tuple))
 print("Multiplied: " + str(answer_tuple[0] * answer_tuple[1] * answer_tuple[2]))
