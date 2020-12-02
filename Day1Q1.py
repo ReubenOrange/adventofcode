4973887e7662029300beceb4c96d40eb903cff27
ReubenOrange <reuben.orange@gmail.com>
2020-12-02 14:19:39 +1100

    Update Day1Q1.py
    Hopefully actually O(N) now

--- Day1Q1.py
+++ Day1Q1.py
@@ -1,17 +1,16 @@
 f = open("input.txt", "r")
 input = f.read()
 split_input_string = input.split()
-split_input_int = [int(i) for i in split_input_string] 
+split_input_int = [int(i) for i in split_input_string]
 
 answer_tuple = (0,0)
-remainder_list =[]
+remainder_set = set()
 
 for x in split_input_int:
-    remainder_list.append(2020-x)
-    for y in remainder_list:
-        if x == y:
-            answer_tuple = (x, 2020-x)
-            break
+    remainder_set.add(2020-x)
+    if x in remainder_set:
+        answer_tuple = (x, 2020-x)
+        break
 
 print("Answer tuple: " + str(answer_tuple))
 print("Multiplied: " + str(answer_tuple[0] * answer_tuple[1]))
