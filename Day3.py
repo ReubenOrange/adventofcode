1679438eb41ca9586739f3fdf6e981889f99bfa3
ReubenOrange <reuben.orange@gmail.com>
2022-12-03 22:19:20 +1100

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,51 +1,46 @@
-import aocd
-import typing as t
+def Day3Q1():
+
+    tree_rows = [row for row in open("input.txt")]
+    trees_hit = 0
+    tree_column = 0
+    last_column = len(tree_rows[0].strip())
+
+    for row in tree_rows:
+        if row[tree_column] == "#":
+            trees_hit += 1
+        tree_column = (tree_column + 3) % last_column
+
+    print("Part 1, trees hit: " + str(trees_hit))
 
 
-def Day2Q1(data: t.List[str]) -> int:
+def Day3Q2():
 
-    priority_sum = 0
+    tree_rows = [row for row in open("input.txt")]
+    slopes = [1, 3, 5, 7, 0.5]
 
-    for rucksack in data:
-        middle = int(len(rucksack) / 2)
-        compartment1 = rucksack[:middle]
-        compartment2 = rucksack[middle:]
+    # dictionary to track trees hit for each slope value
+    trees_hit_per_slope = {1: 0, 3: 0, 5: 0, 7: 0, 0.5: 0}
+    tree_column = 0
+    last_column = len(tree_rows[0].strip())
 
-        for item in compartment1:
-            if item in compartment2:
-                if ord(item) > 96:
-                    priority_sum += ord(item) - 96
-                else:
-                    priority_sum += ord(item) - 64 + 26
-                break
+    for slope in slopes:
+        for row in tree_rows:
+            # check tree_column is a whole number (for decimal slopes)
+            if tree_column % 1 == 0:
+                if row[int(tree_column)] == "#":
+                    trees_hit_per_slope[slope] += 1
+            tree_column = (tree_column + slope) % last_column
+        tree_column = 0
 
-    return priority_sum
+    # multiply trees hit together for each slope
+    trees_hit_multi = 1
+    for slope in trees_hit_per_slope:
+        print("Slope: " + str(slope) + ", Trees hit: " + str(trees_hit_per_slope[slope]))
+        trees_hit_multi = trees_hit_multi * trees_hit_per_slope[slope]
 
-
-def Day2Q2(data: t.List[str]) -> int:
-
-    priority_sum = 0
-
-    for i in range(len(data)):
-
-        if i % 3 == 2:
-            for item in data[i]:
-                if (item in data[i - 1]) and (item in data[i - 2]):
-                    if ord(item) > 96:
-                        priority_sum += ord(item) - 96
-                    else:
-                        priority_sum += ord(item) - 64 + 26
-                    break
-
-    return priority_sum
+    print("Part 2, trees hit:" + str(trees_hit_multi))
 
 
 if __name__ == "__main__":
-
-    data = aocd.get_data(
-        day=3,
-        year=2022,
-    ).splitlines()
-
-    print("Part 1:", Day2Q1(data))
-    print("Part 2:", Day2Q2(data))
+    Day3Q1()
+    Day3Q2()
