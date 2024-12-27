3c441489abe5a5628da1a98a4f7170c7ecc4ec06
= <=>
2024-12-27 12:10:37 +1100

    Day1

--- 2024/Day1.py
+++ 2024/Day1.py
@@ -1,42 +1,49 @@
-f = open("Day1Input.txt", "r")
-
-sum = 0
-x1_list = []
-x2_list = []
-
-for line in f.readlines():
-    # x1, x2 = 
-    x1, x2 = line.split()
-    x1_list += [int(x1)]
-    x2_list += [int(x2)]
-    # print(x1, x2)
-
-x1_list.sort()
-x2_list.sort()
-
-for x1, x2 in zip(x1_list, x2_list):
-    sum += abs(x1 - x2)
-    
-print(sum)
+import aocd
+import typing as t
 
 
+def Q1(lines: t.List[str]) -> int:
+
+    sum = 0
+    x1_list = []
+    x2_list = []
+
+    for line in lines:
+        x1, x2 = line.split()
+        x1_list += [int(x1)]
+        x2_list += [int(x2)]
+
+    x1_list.sort()
+    x2_list.sort()
+
+    for x1, x2 in zip(x1_list, x2_list):
+        sum += abs(x1 - x2)
+
+    return sum
 
 
-f = open("Day1Input.txt", "r")
+def Q2(lines: t.List[str]) -> int:
 
-x1_list = []
-x2_list = []
+    sim_score = 0
+    x1_list = []
+    x2_list = []
 
-for line in f.readlines():
-    x1, x2 = line.split()
-    x1_list += [int(x1)]
-    x2_list += [int(x2)]
+    for line in lines:
+        x1, x2 = line.split()
+        x1_list += [int(x1)]
+        x2_list += [int(x2)]
 
-sim_score = 0
+    for x1 in x1_list:
+        for x2 in x2_list:
+            if x1 == x2:
+                sim_score += x1
 
-for x1 in x1_list:
-    for x2 in x2_list:
-        if x1 == x2:
-            sim_score += x1
-    
-print("similarity score: ", sim_score)
+    return sim_score
+
+
+if __name__ == "__main__":
+
+    lines = aocd.get_data(day=1, year=2024).split("\n")
+
+    print("Part 1:", Q1(lines))
+    print("Part 2:", Q2(lines))
